e6f4dbc587cc605cd1a5663e334d74c138167e69
Naman Arora <namanarora0303@gmail.com>
2026-04-24 23:50:25 +0530

    Fix API base URL

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL
+  baseURL: process.env.REACT_APP_API_URL + '/api'
 });
-
 api.interceptors.request.use(config => {
   const token = localStorage.getItem('webdesk_token');
   if (token) config.headers.Authorization = `Bearer ${token}`;
